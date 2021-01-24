3e43c9c165f6754b1ae17a10ed25d5cc5119ecfe
Phil Roffe <philroffe@gmail.com>
2021-01-24 10:21:28 +0000

    Big improvements to create-game - dynamic text boxes and improved save coords capability

--- public/quiz-data.js
+++ public/quiz-data.js
@@ -1,3 +1,9 @@
+var quizHistory = [
+{"date": "2020-12-18", "id": "243969", "description": "Capital Cities"},
+{"date": "2021-01-03", "id": "243969", "description": "Liverpool Players Birthplace"},
+{"date": "2021-01-18", "id": "241296", "description": "James Bond Locations"},
+];
+
 var liverpoolPlayersBirthList = [
 ["Alisson Becker", "Novo Hamburgo, State of Rio Grande do Sul, Brazil"],
 ["Virgil Van Dijk", "Breda, Netherlands"],
@@ -16,3 +22,4 @@ var liverpoolPlayersBirthList = [
 ["Diogo Jota", "Massarelos, Porto, Portugal"],
 ["Divock Origi", "Ostend, Belgium"],
 ];
+
